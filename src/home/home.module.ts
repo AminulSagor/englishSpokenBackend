851abc340f656049daba8c360a6cd32980bdb8e7
AminulSagor <aminulislamsagor3@gmail.com>
2024-08-13 20:08:42 +0600

    filterdto handled6

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -3,10 +3,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HomeService } from './home.service';
 import { HomeGateway } from './home.gateway';
+import { AuthSharedModule } from '../auth/auth-shared.module';
 import { User } from 'src/users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthSharedModule, 
+  ],
   providers: [HomeService, HomeGateway],
 })
 export class HomeModule {}
